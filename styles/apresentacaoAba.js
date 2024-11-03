08256be049b4c726b183f28739d54d7fa187d4d3
Matheus <123899818+MatheusNoronhadaSilva@users.noreply.github.com>
2024-11-02 22:16:17 -0300

    teste

---
+++ styles/apresentacaoAba.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ApresentacaoAba = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: end;
+   gap: 36%;
+   width: 100%;
+`
+
+export default ApresentacaoAba
